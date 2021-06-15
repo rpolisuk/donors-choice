3b83fe142d5bb1c95c9cfb29887eaafba9ca5624
mjshah5 <malavshah642001@gmail.com>
2021-06-15 17:52:32 -0400

    FAQ Page Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { FaqComponent } from './faq/faq.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'about', component: AboutComponent},
+  {path: 'faq', component: FaqComponent},
   // {path: '**', component: PageNotFoundComponent}
 ];
 
